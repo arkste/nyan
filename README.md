f6e0d8e00bb8cbfc8a44141e62a7a7af144528c9
Arkadius Stefanski <arkste@users.noreply.github.com>
2020-01-30 15:26:49 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# nyan
+
+This is a wrapper around `npm` & `yarn`.
+
+It checks, if `package-lock.json` or `yarn.lock` exists, otherwise it will output an error message.
+
+This is just a PoC how it *could* be done.
+
+[This tweet](https://twitter.com/tesseralis/status/1222784049686564864) is the reason this small program exists.
+
+## Installation
+
+Grab the binary from the releases tab.
+
+## Usage
+
+    $ nyan version
+
+## License
+
+nyan is released under the [MIT License](http://www.opensource.org/licenses/MIT).
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	npmLockFile = "package-lock.json"
+	npmCommand = "npm"
+
+	yarnLockFile = "yarn.lock"
+	yarnCommand = "yarn"
+)
+
+func main() {
+	args := os.Args[1:]
+	log.SetLevel(log.FatalLevel)
+
+	if _, err := os.Stat(npmLockFile); !os.IsNotExist(err) { // check if npm lock exists in current directory
+		log.Info("npm lock file found")
+
+		// npm knows about "add", no need to replace with "install"
+		cmd := exec.Command(npmCommand, args...)
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Error(err)
+		}
+		fmt.Printf("%s", out)
+
+	} else if _, err := os.Stat(yarnLockFile); !os.IsNotExist(err) { // check if yarn lock exists in current directory
+		log.Info("yarn lock file found")
+
+		// replace install only if there's another argument/package name
+		if len(args) > 1 {
+			if args[0] == "install" {
+				args[0] = "add"
+			}
+		}
+
+		cmd := exec.Command(yarnCommand, args...)
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Error(err)
+		}
+		fmt.Printf("%s", out)
+	} else { // throw error if there's no npm or yarn lock file in the current directory
+		log.Fatal("no npm/yarn lock file found")
+	}
+}
